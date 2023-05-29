bbc0892f1424f313761348c61fd87779031d2991
Lila Ratnikova <enilily@ya.ru>
2023-05-29 11:53:24 +0600

    исправления

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
@@ -24,7 +24,7 @@ router.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
