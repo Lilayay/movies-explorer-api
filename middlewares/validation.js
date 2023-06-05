82d6f208ffc81b64b1942a3ba5356e369f0a849c
Lila Ratnikova <enilily@ya.ru>
2023-06-05 11:50:19 +0600

    delete movie

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -19,7 +19,7 @@ module.exports.movieValidation = celebrate({
 
 module.exports.movieIdValidation = celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().length(24).hex().required(),
+    _id: Joi.string().hex().length(24).required(),
   }),
 });
 
